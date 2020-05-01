e7138206147caf62d599de644aea994291c4e457
tanjimTC <tanjimtc74@gmail.com>
2020-05-02 04:12:28 +0600

    card added

--- src/componenets/Product/Product.js
+++ src/componenets/Product/Product.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./Product.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
   const { name, img, seller, price, stock } = props.product;
   return (
     <div className="product">
       <div className="product-image">
-            <img src={img} alt="" />
+        <img src={img} alt="" />
       </div>
       <div className="product-desc">
-            <h4 className="product-name">{name}</h4>
-            <br />
-            <p>
-            <small>{seller}</small>
-            </p>
-            <p>${price}</p>
-            <p>only {stock} left in stock - order soon</p>
+        <h4 className="product-name">{name}</h4>
+        <p>
+         <b> Seller</b> :<small> {seller}</small>
+        </p>
+        <p><b>Price</b> : ${price}</p>
+        <p>only {stock} left in stock - order soon</p>
+        <button onClick={()=>props.handleAddProduct(props.product)} className="cart-button">
+        <i class="fas fa-shopping-cart">  </i> 
+          add to cart
+        </button>
       </div>
     </div>
   );
--- src/componenets/shop/Shop.js
+++ src/componenets/shop/Shop.js
@@ -8,15 +8,30 @@ const Shop = () => {
 
   const [products, setProduts] = useState(firstTen);
 
+  const [cart, setCart] = useState([]);
+
+  const handleAddProduct = (product) => {
+    console.log("add clicked", product);
+    const newCart = [...cart ,product ];
+    setCart(newCart);
+  };
+
   return (
     <div className="shop-container">
       <div className="product-container">
-        {
-            products.map((x) =><Product product={x}></Product>)
-        }
+        {products.map((x) => (
+          <Product
+            key={Math.floor(Math.random() * 1000)}
+            product={x}
+            handleAddProduct={handleAddProduct}
+          ></Product>
+        ))}
       </div>
       <div className="cart-container">
-        <h5>I am cart</h5>
+        <h3>This is cart</h3>
+        <h4>
+          Order summary :<i class="fas fa-shopping-cart"></i> {cart.length}
+        </h4>
       </div>
     </div>
   );
