e7f1f842369195d909b13f9a63c913374bf7024b
tanjimTC <tanjimtc74@gmail.com>
2020-05-12 16:48:46 +0600

    Ema john site updated

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { getDatabaseCart, removeFromDatabaseCart ,processOrder} from "../../utilities/databaseManager";
+import {
+  getDatabaseCart,
+  removeFromDatabaseCart,
+  processOrder,
+} from "../../utilities/databaseManager";
 import fakeData from "../../fakeData";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import Cart from "../Cart/Cart";
+import happyImage from "../../images/giphy.gif";
 
 const Review = () => {
   const [cart, setCart] = useState([]);
+  const [orderPlaced, setOrderPlaced] = useState(false);
 
-  const removeProduct = (productKey) =>{
-    const newCart = cart.filter(x=>x.key!==productKey);
+  const removeProduct = (productKey) => {
+    const newCart = cart.filter((x) => x.key !== productKey);
     setCart(newCart);
     removeFromDatabaseCart(productKey);
-  }
+  };
 
-  const handlePlaceOrder = ()=>{
+  const handlePlaceOrder = () => {
     setCart([]);
+    setOrderPlaced(true);
     processOrder();
-  }
-
+  };
 
   useEffect(() => {
     const savedCart = getDatabaseCart();
@@ -27,26 +33,32 @@ const Review = () => {
       product.quantity = savedCart[key];
       return product;
     });
-    setCart(cardProduct)
+    setCart(cardProduct);
   }, []);
   return (
-      <div className='shop-container'>
-          <div className="product-container">
-          {
-            cart.map(x=><ReviewItem 
-              key={x.key} 
-              product={x}
-              removeProduct={removeProduct}
-              />)
-          }
-          </div>
-          <div className="cart-container">
-          <Cart cart={cart}>
-            <button onClick={handlePlaceOrder} className="cart-button review">Place Order</button>
-          </Cart>
-          </div>
+    <div className="shop-container">
+      <div className="product-container">
+        {cart.map((x) => (
+          <ReviewItem key={x.key} product={x} removeProduct={removeProduct} />
+        ))}
+        {orderPlaced ? (
+          <center>
+            <h2>Order Placed Successfully!!</h2>
+            <img src={happyImage} alt="" />
+          </center>
+        ) : (
+          ""
+        )}
       </div>
-    );
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={handlePlaceOrder} className="cart-button review">
+            Place Order
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
